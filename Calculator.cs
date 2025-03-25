228f8e1a13af9b0761e30cca055efa0061f37f21
Róbert Zámočník <zamocnikrobert@gmail.com>
2025-03-25 02:02:07 +0100

    Initial commit

---
+++ Calculator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Statistika
+{
+    public class Calculator
+    {
+        private List<double> Numbers { get; set; } = new List<double>();
+
+        public Calculator(string input)
+        {
+           
+            SetInput(input);
+        }
+
+        public void SetInput(string input)
+        {
+         
+            var parts = input.Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries);
+            Numbers = parts.Select(p => double.Parse(p)).ToList();
+        }
+
+        public double GetSum() => Numbers.Sum();
+        public double GetAverage() => Numbers.Average();
+        public double GetMin() => Numbers.Min();
+        public double GetMax() => Numbers.Max();
+        public string GetOrderedStringAsc() => string.Join(" ", Numbers.OrderBy(n => n));
+        public string GetOrderedStringDsc() => string.Join(" ", Numbers.OrderByDescending(n => n));
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Statistika
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Calculator calculator;
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+
+       
+
+        private void biggestNumberTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            calculator = new Calculator(inputForNumbers.Text);
+            sumTextBox.Text = calculator.GetSum().ToString();
+            avgTextBox.Text = calculator.GetAverage().ToString();
+            lowestNumberTextBox.Text = calculator.GetMin().ToString();
+            biggestNumberTextBox.Text = calculator.GetMax().ToString();
+            orderedTextBox.Text = calculator.GetOrderedStringAsc();
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            // Pokud ještě nebyl vytvořen kalkulátor, vytvoř ho
+            if (calculator == null)
+            {
+                if (string.IsNullOrWhiteSpace(inputForNumbers.Text)) return;
+                calculator = new Calculator(inputForNumbers.Text);
+            }
+
+            if (comboBox.SelectedIndex == 0)
+            {
+                orderedTextBox.Text = calculator.GetOrderedStringAsc();
+            }
+            else if (comboBox.SelectedIndex == 1)
+            {
+                orderedTextBox.Text = calculator.GetOrderedStringDsc();
+            }
+        }
+
+        private void inputForNumbers_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
